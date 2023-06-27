cb2e21cba6f9a9ce88b6c83bfd1b643b8c5dde94
Arim <dkflarnjs11@gmail.com>
2023-06-27 11:08:24 +0900

    매안뷰

---
+++ FirstProject/FirstView/MainView.swift
@@ -0,0 +1,45 @@
+//
+//  MainView.swift
+//  Lifesum
+//
+//  Created by 권아림 on 2023/06/19.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        
+        ZStack{
+            
+            Color(hex: "255A46")
+                .ignoresSafeArea()
+            
+            HStack{
+                
+                Text("Lifesum")
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                
+                ZStack{
+                    
+                    Image(systemName: "circle")
+                        .foregroundColor(.white)
+                    
+                    Text("R")
+                        .font(.system(size: 10))
+                        .foregroundColor(Color.white)
+                }
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+
+
